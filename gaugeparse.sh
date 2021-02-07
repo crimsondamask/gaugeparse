19a02b49ef879a994e257305f0180c6b384df6a2
Abdelkader Madoui <abdelkadermadoui@gmail.com>
2021-02-07 01:00:19 +0100

    first commit

--- gaugeparse.sh
+++ gaugeparse.sh
@@ -3,12 +3,9 @@
 starttime="$2"
 step="$3"
 
-
-
 while read -r line;
 do
     timed=$( date -d "+ $step seconds $starttime" +"%Y/%m/%d %H:%M:%S" )
-    echo "$timed"
-    echo "$timed     $line" >> output.txt
+    echo "$timed     $line" 1>> output.txt
     starttime=$timed
-done < "$1"
+done < <(cat $1)
