790642cfe2fcfe3c8746348a9f5501227f68a2fb
crimsondamask <abdelkadermadoui@gmail.com>
2021-02-07 19:33:14 +0000

    cleaned the script

--- gaugeparse.sh
+++ gaugeparse.sh
@@ -3,12 +3,9 @@
 starttime="$2"
 step="$3"
 
-
-
 while read -r line;
 do
     timed=$( date -d "+ $step seconds $starttime" +"%Y/%m/%d %H:%M:%S" )
-    echo "$timed"
     echo "$timed     $line" >> output.txt
     starttime=$timed
 done < "$1"
