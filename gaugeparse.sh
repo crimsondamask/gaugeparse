9527949fd34ec1496c5385fc897bfeca92cdbe0d
Abdelkader Madoui <abdelkadermadoui@gmail.com>
2021-04-20 21:44:34 +0100

    Status message

--- gaugeparse.sh
+++ gaugeparse.sh
@@ -3,9 +3,14 @@
 starttime="$2"
 step="$3"
 
+numlines=$( wc -l < $1 )
+count=0
+
 while read -r line;
 do
     timed=$( date -d "+ $step seconds $starttime" +"%Y/%m/%d %H:%M:%S" )
     echo "$timed     $line" 1>> output.txt
     starttime=$timed
-done < <(cat $1)
+    count=$((count+1))
+    echo "Writing line $count/$numlines"
+done <$1
